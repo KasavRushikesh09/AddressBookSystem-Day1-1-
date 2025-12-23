ee00ee9a7f199a2984943ec73f78a473ca6d835f
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-12-23 15:38:46 +0530

    inital commit

--- src/AddressBookSystem/AddressBook.java
+++ src/AddressBookSystem/AddressBook.java
@@ -1,4 +1,44 @@
 package AddressBookSystem;
 
+import java.util.ArrayList;
+
 public class AddressBook {
+
+    private ArrayList<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+        System.out.println("Contact added successfully");
+    }
+
+    public void editContact(String firstName, String newCity) {
+        for (Contact c : contacts) {
+            if (c.getFirstName().equalsIgnoreCase(firstName)) {
+                c.setCity(newCity);
+                System.out.println("Contact updated");
+                return;
+            }
+        }
+        System.out.println("Contact not found");
+    }
+    public void deleteContact(String firstName) {
+        for (Contact c : contacts) {
+            if (c.getFirstName().equalsIgnoreCase(firstName)) {
+                contacts.remove(c);
+                System.out.println("Contact deleted");
+                return;
+            }
+        }
+        System.out.println("Contact not found");
+    }
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("No contacts available");
+            return;
+        }
+        for (Contact c : contacts) {
+            System.out.println(c);
+        }
+    }
 }
+
