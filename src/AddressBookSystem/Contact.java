340fe0dd1bfd61da7f44335bfa58bf70cb644978
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-12-23 15:19:09 +0530

    Initial commit

--- src/AddressBookSystem/Contact.java
+++ src/AddressBookSystem/Contact.java
@@ -1,4 +1,41 @@
 package AddressBookSystem;
 
 public class Contact {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String email;
+
+    public Contact(String firstName, String lastName, String address,
+                   String city, String state, String zip,
+                   String phone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName +
+                ", " + city +
+                ", " + phone +
+                ", " + email;
+    }
 }
