374a55d079791a5c3889cf3c1361ef6cd3696c99
KasavRushikesh09 <rishikeshkasav2003@gmail.com>
2025-12-23 15:56:43 +0530

    Address Book Manager

--- src/AddressBookSystem/AddressBookManager.java
+++ src/AddressBookSystem/AddressBookManager.java
@@ -1,4 +1,21 @@
 package AddressBookSystem;
 
+import java.util.HashMap;
+
 public class AddressBookManager {
+
+    private HashMap<String, AddressBook> books = new HashMap<>();
+
+    public void addAddressBook(String name) {
+        if (!books.containsKey(name)) {
+            books.put(name, new AddressBook());
+            System.out.println("Address Book created: " + name);
+        } else {
+            System.out.println("Address Book already exists");
+        }
+    }
+
+    public AddressBook getAddressBook(String name) {
+        return books.get(name);
+    }
 }
